c16a866edf43fc17b50aa0defb0a75a57e5de381
Samir Marin <marin.samir@gmail.com>
2018-12-01 13:49:57 -0800

    Initial Commit

---
+++ pick/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pick
+//
+//  Created by Samir Marin on 2018-12-01.
+//  Copyright © 2018 Pick Application. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
